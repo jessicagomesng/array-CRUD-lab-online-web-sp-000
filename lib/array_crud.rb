531bed57d580a264131fb78b4ae663f54bc3ccda
Jessica Gomes-Ng <jessicagomesng@gmail.com>
2020-03-26 13:56:14 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,7 +8,7 @@ end
 
 def add_element_to_end_of_array(array, element)
   dogs = ["Peety", "Ronnie", "Reggie", "Pepper"]
-  
+  dogs << "Skittles"
 end
 
 def add_element_to_start_of_array(array, element)
