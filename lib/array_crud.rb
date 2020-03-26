29107a67651d798d3134007125e8f1596bf39078
Jessica Gomes-Ng <jessicagomesng@gmail.com>
2020-03-26 13:59:05 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -14,8 +14,8 @@ end
 
 def add_element_to_start_of_array(array, element)
   array = ["Peety", "Ronnie", "Reggie", "Pepper"]
-  dogs.unshift("Skittles")
-  dogs
+  array.unshift("#{element}")
+  array
 end
 
 def remove_element_from_end_of_array(array)
